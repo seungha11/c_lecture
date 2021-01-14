a5c074c5358fb609be11076356d248b3ea7b37d9
User <User@BCLASS-06>
2021-01-14 19:19:55 +0900

    대입연산 연습

--- Day4/Day4/01.c
+++ Day4/Day4/01.c
@@ -2,6 +2,24 @@
 
 int main(void)
 {
+	int age; //변수선언
+	age = 10; //대입연산
+
+	int age2 = 10; //변수선언과 대입을 동시에
+	
+	int a = 10;
+	int b = 20;
+	int c = 30;
+
+	int a, b, c;
+	a = 10;
+	b = 20;
+	c = 30;
+
+	int a_1 = 10, b_2 = 20, c_2 = 30;
+
+	printf("%d\n", age);
+
 
 	return 0;
 }
